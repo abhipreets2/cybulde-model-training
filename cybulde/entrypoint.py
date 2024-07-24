faecb427957ad036178c9f8065748eaf28123a2d
abhipreets2 <abhipreets2@gmail.com>
2024-07-24 13:50:30 +0530

    added docker and makefile

--- cybulde/entrypoint.py
+++ cybulde/entrypoint.py
@@ -6,13 +6,14 @@ from cybulde.utils.config_utils import get_config
 from cybulde.utils.utils import get_logger
 from cybulde.config_schemas.config_schema import Config
 
+
 @get_config(config_path="../configs", config_name="config")
 def entrypoint(config: Config) -> None:
     os.environ["HYDRA_FULL_ERROR"] = "1"
     logger = get_logger(__file__)
     print(OmegaConf.to_yaml(config))
     
-    instance_group_creator = instantiate(config.infrastructure.instance_group_creator)
-    instance_ids = instance_group_creator.launch_instance_group()
+    #instance_group_creator = instantiate(config.infrastructure.instance_group_creator)
+    #instance_ids = instance_group_creator.launch_instance_group()
 if __name__ == "__main__":
     entrypoint()
